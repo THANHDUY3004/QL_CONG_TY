83af4640337e47c7a924fd36bb450bff40552b36
ThanhDuy <huynhduy335364@gmail.com>
2025-05-23 21:32:59 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QL_CONG_TY.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult PhongBan()
+        {
+            return View();
+        }
+        public ActionResult ChucVu()
+        {
+            return View();
+        }
+        public ActionResult NhanSu()
+        {
+            return View();
+        }
+        public ActionResult HopDong()
+        {
+            return View();
+        }
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Chưa có thông tin";
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QL_CONG_TY.MvcApplication" Language="C#" %>
